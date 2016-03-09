cec893029b1d4fbec1f5e7abaef58aef1d8b3187
Daniel P. McAllaster <dmcallaster@is-llc.net>
2016-03-09 09:37:41 -0800

    Attempting to make instance restart pull in JBoss environment so it will run properly.

--- modules/gsajboss6/manifests/util/restart.pp
+++ modules/gsajboss6/manifests/util/restart.pp
@@ -1,4 +1,6 @@
 define gsajboss6::util::restart ($ensure, $instance = $title) {
+  include stdlib
+
   if ($ensure == 'present') {
     exec { "start_${instance}":
       command     => '/bin/true',
@@ -8,8 +10,22 @@ define gsajboss6::util::restart ($ensure, $instance = $title) {
       refreshonly => true,
     }
 
+
+    $hipchat_notify = hiera('hipchat::notify', false)
+
+    if $hipchat_notify {
+      $hipchat_room = uriescape(hiera('hipchat::room', 'JBoss 6 Upgrade'))
+      $hipchat_apikey = hiera('hipchat::apikey', '88068e6d406ca3a8eea31c92c35da7')
+      exec { "hc_notify_restart_${instance}":
+        command     => "/usr/bin/curl -x https://squid:3128 -output /dev/null -d \"room_id=${hipchat_room}&from=Puppet&message=Restarting+instance+${instance}&color=green&notify=1\" \"https://api.hipchat.com/v1/rooms/message?format=json&auth_token=${hipchat_apikey}\"",
+        refreshonly => true,
+        before      => Exec["restart_${instance}"],
+      }
+
+    }
+
     exec { "restart_${instance}":
-      command     => "/opt/sw/jboss/rc_scripts/restart_jboss_${instance}.sh",
+      command     => "/bin/su - jboss --command='/opt/sw/jboss/rc_scripts/restart_jboss_${instance}.sh'",
       refreshonly => true,
       logoutput   => on_failure,
       user        => 'jboss',
