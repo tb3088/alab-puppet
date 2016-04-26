aced9eed01d7214b2a03be0667b1ebc7782ed1f4
Daniel P. McAllaster <dmcallaster@is-llc.net>
2016-04-26 16:16:40 -0700

    adding environment file

---
+++ production/manifests/site.pp
@@ -0,0 +1,13 @@
+## This part is just to supress a warning. Continue on for the main portions of the file
+# Turn off virtual packages unless overridden by Hiera:
+if versioncmp($::puppetversion,'3.6.1') >= 0 {
+  $allow_virtual_packages = hiera('allow_virtual_packages',false)
+
+  Package {
+    allow_virtual => $allow_virtual_packages,
+  }
+}
+
+File {
+  backup => false,
+}
