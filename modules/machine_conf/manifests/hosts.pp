dd3293b9c041438bb042e46dd2710b8965bf3172
Daniel P. McAllaster <dmcallaster@is-llc.net>
2016-05-05 10:08:15 -0700

    Updated hosts file class to permit changing of IP addresses

--- modules/machine_conf/manifests/hosts.pp
+++ modules/machine_conf/manifests/hosts.pp
@@ -1,7 +1,14 @@
-# Module to install set up for local development
+# Configure the hosts file for a vLab JBoss server machine.
 
-class machine_conf::hosts {
+class machine_conf::hosts (
+  $dmz_subnet_base     = '172.22.10',
+  $backend_subnet_base = '172.22.11',
+)
+{
   require stdlib
+
+  validate_ip_address("${dmz_subnet_base}.0")
+  validate_ip_address("${backend_subnet_base}.0")
 
   host { $::fqdn:
     ip           => $::ipaddress,
@@ -9,43 +16,43 @@ class machine_conf::hosts {
   }
 
   host { 'lab5-portal.fas.gsarba.com':
-    ip           => '172.22.10.4',
+    ip           => "${dmz_subnet_base}.4",
     host_aliases => 'assist-internal.fas.gsarba.com',
   }
   host { 'Lab5-was.itss.gsarba.com':
-    ip           => '172.22.10.5',
+    ip           => "${dmz_subnet_base}.5",
     host_aliases => ['Lab5-apachePrx2','Lab5-apachePrx2.itss.gsarba.com','Lab5-was','loadbalancer-int.itss.gsarba.com','loadbalancer_int.itss.gsarba.com','tos','tos.fas.gsarba.com'],
   }
   host { 'Lab5-domExtran1.fas.gsarba.com':
-    ip           => '172.22.10.8',
+    ip           => "${dmz_subnet_base}.8",
     host_aliases => ['Lab5-domExtran1','Lab5-tos.fas.gsarba.com','domextran2','domextran2.fas.gsarba.com','domino','domino.fas.gsarba.com']
   }
   host { 'Lab5-reports.fas.gsarba.com':
-    ip           => '172.22.11.19',
+    ip           => "${backend_subnet_base}.19",
     host_aliases => 'lab5-ofm.fas.gsarba.com',
   }
   host { 'lab5-domphobos.fas.gsarba.com':
-    ip           => '172.22.11.28',
+    ip           => "${backend_subnet_base}.28",
     host_aliases => 'lab5-domphobos',
   }
   host { 'Lab5-nbaOMIS.fas.gsarba.com':
-    ip           => '172.22.11.27',
+    ip           => "${backend_subnet_base}.27",
     host_aliases => 'Lab5-nbaOMIS',
   }
   host { 'Lab5-rbaCODB.fas.gsarba.com':
-    ip           => '172.22.11.20',
+    ip           => "${backend_subnet_base}.20",
     host_aliases => ['Lab5-rbaCODB','Lab5-rbaCODB.itss.gsarba.com'],
   }
   host { 'lab5-splunkpuppet.fas.gsarba.com':
-    ip           => '172.22.11.23',
+    ip           => "${backend_subnet_base}.23",
     host_aliases => ['puppet','squid','puppet.fas.gsarba.com']
   }
   host { 'Lab5-AD.gsarba.com':
-    ip           => '172.22.11.29',
+    ip           => "${backend_subnet_base}.29",
     host_aliases => 'Lab5-AD',
   }
   host { 'vlab-mailserver.fas.gsarba.com':
-    ip           => '172.22.11.30',
+    ip           => "${backend_subnet_base}.30",
     host_aliases => 'vlab-mailserver',
   }
 
