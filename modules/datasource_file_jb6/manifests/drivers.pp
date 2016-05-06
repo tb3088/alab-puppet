b7d24308607eaf23b05ba7fa6e0e335add7cb68c
Daniel P. McAllaster <dmcallaster@is-llc.net>
2016-05-06 11:07:52 -0700

    Remove database drivers we are not going to be using.

--- modules/datasource_file_jb6/manifests/drivers.pp
+++ modules/datasource_file_jb6/manifests/drivers.pp
@@ -4,19 +4,27 @@ define datasource_file_jb6::drivers($instance) {
 
   $file = "/opt/sw/jboss/gsaconfig/instances/${instance}/server/instanceconfig/configuration/${instance}.xml"
 
+  augeas { "${title}-remove-old-oracle-drivers":
+    lens    => 'Xml.lns',
+    incl    => $file,
+    changes => [
+      "rm server//subsystem/datasources/drivers/driver[#attribute/name='ojdbc14']",
+      "rm server//subsystem/datasources/drivers/driver[#attribute/name='ojdbc6']",
+    ],
+  }
+
+  augeas { "${instance}-remove-h2-driver":
+    lens    => 'Xml.lns',
+    incl    => "/opt/sw/jboss/gsaconfig/instances/${instance}/server/instanceconfig/configuration/${instance}.xml",
+    changes => [
+      "rm  server//subsystem/datasources/drivers/driver[#attribute/name='h2']",
+    ],
+  }
+
   augeas { "${title}-oracle-drivers":
     lens    => 'Xml.lns',
     incl    => $file,
     changes => [
-
-      "set server//subsystem/datasources/drivers/driver[#attribute/name='ojdbc14']/#attribute/name ojdbc14",
-      "set server//subsystem/datasources/drivers/driver[#attribute/name='ojdbc14']/#attribute/module oracle.jdbc:14",
-      "set server//subsystem/datasources/drivers/driver[#attribute/name='ojdbc14']/driver-class/#text oracle.jdbc.driver.OracleDriver",
-
-      "set server//subsystem/datasources/drivers/driver[#attribute/name='ojdbc6']/#attribute/name ojdbc6",
-      "set server//subsystem/datasources/drivers/driver[#attribute/name='ojdbc6']/#attribute/module oracle.jdbc:6",
-      "set server//subsystem/datasources/drivers/driver[#attribute/name='ojdbc6']/driver-class/#text oracle.jdbc.driver.OracleDriver",
-
       "set server//subsystem/datasources/drivers/driver[#attribute/name='ojdbc7']/#attribute/name ojdbc7",
       "set server//subsystem/datasources/drivers/driver[#attribute/name='ojdbc7']/#attribute/module oracle.jdbc:7",
       "set server//subsystem/datasources/drivers/driver[#attribute/name='ojdbc7']/driver-class/#text oracle.jdbc.driver.OracleDriver",
