9dc2245bb80d91a2ab1dc405154260d921f85eb1
Matthew Patton <matthew.patton@itgfirm.com>
2018-03-03 12:18:05 -0500

    support JSON format

--- modules/bootstrap/files/facter/facts.d/s3_buckets.sh
+++ modules/bootstrap/files/facter/facts.d/s3_buckets.sh
@@ -5,41 +5,39 @@ function runv() {
     "$@"
 }
 
-[ $# -eq 1 ] && { key="$1"; shift; }
 set -eo pipefail
 
 curl='curl --silent --fail'
 meta_url='http://169.254.169.254/latest/meta-data'
 : ${REGION:=`$curl $meta_url/placement/availability-zone | sed -e 's/[a-z]$//'`}
-: ${INSTANCE_ID:=`$curl $meta_url/instance-id`}
+#instance_id=`$curl $meta_url/instance-id`
 
 : ${PREFIX:=`basename $0 .sh`}
 : ${FORMAT:=text}
 
 case "${FORMAT,,}" in
-    text)
-        awk_printf='"%s.%s=\x27%s\x27\n", prefix, tolower($2), $5'
+    text)  
+        awk_begin='BEGIN { printf("%s=[ ", prefix); }'
+        awk_printf='"\x27%s\x27, ", $3'
+        awk_end='END { printf("]\n"); }'
         ;;&
     json)
         post_format="|python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)'"
         ;&
-    yaml) 
+    yaml)
         awk_begin='BEGIN { printf("---\n%s:\n", prefix); }'
 
         # wrap in single-quotes, because YAML parser
-        awk_printf='"  %s: \x27%s\x27\n", tolower($2), $5'
+        awk_printf='"  - \x27%s\x27\n", $3'
         ;&
     text|yaml|json)
-        FORMAT=text
         # NOTE - 'filter' and 'key' are by definition mutually exclusive!
         read -r -d '' cmd_awk <<_EOF || true
 |awk -v prefix='$PREFIX' -v filter='$FILTER' -v key='$key' '
     $awk_begin
-    \$2 ~ filter {
-        if (\$2 ~ key) { quit=1; }
-        gsub(/[^-[:alnum:]._:]/, "_", \$2)
+    \$3 ~ filter {
+        if (length(key) != 0) { \$3 = \$1; \$1 = key; }
         printf($awk_printf)
-        if (quit == 1) { exit; }
     }
     $awk_end
 '
@@ -48,11 +46,12 @@ _EOF
     *)  >&2 echo "unsupported format ($FORMAT)"; exit 1
 esac
 
-# '--debug' screws up output so DO NOT use except interactive
-cmd="aws ${PROFILE+ --profile $PROFILE} --region ${REGION:?} --output ${FORMAT:?}"
-cmd+=" ec2 describe-tags --filters 'Name=resource-id,Values=${INSTANCE_ID:?}'"
 
-${DEBUG+runv} eval "$cmd $cmd_awk $post_format"
+# '--debug' screws up output so DO NOT use except interactive
+cmd="aws ${PROFILE+ --profile $PROFILE} --region ${REGION:?}"
+cmd+=" s3 ls s3://"
+                
+${DEBUG+runv} eval "$cmd $@ $cmd_awk $post_format"
 
 
 
--- modules/bootstrap/files/facter/puppet.sh
+++ modules/bootstrap/files/facter/puppet.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
-#set -eo pipefail
 
 function runv() {
     1>&2 echo "+ $*"
     "$@"
 }
 
-[ $# -eq 1 ] && key=$1
+# Only when ARGS is a single item is $cmd output reduced to just the value
+[ $# -eq 1 ] && { key="$1"; shift; }
+set -eo pipefail
+
+: ${PREFIX:=`basename $0 .sh`}
+: ${FORMAT:=text}
 
 case "${FORMAT,,}" in
+    text)
+        awk_printf='"%s.%s=\x27%s\x27\n", prefix, $1, $3'
+        ;;&
+    json)
+        post_format="|python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)'"
+        ;&
     yaml)
         # embedded ':' without whitespace should be safe. Else try 'value'.
         # https://stackoverflow.com/questions/11301650/how-to-escape-indicator-characters-i-e-or-in-yaml/22483116
@@ -19,25 +29,27 @@ case "${FORMAT,,}" in
 
         awk_begin='BEGIN { printf("---\n%s:\n", prefix); }'
 
-        # wrap values in single-quotes because YAML parser
+        # wrap in single-quotes, because YAML parser
         awk_printf='"  %s: \x27%s\x27\n", $1, $3'
-        : ${PREFIX:=`basename $0 .sh`}
-        ;;
-    *)  awk_printf='"%s%s=%s\n", prefix, $1, $3'
-        : ${PREFIX:=`basename $0 .sh`.}
-esac
-
-# '--debug' screws up output so DO NOT use except interactive
-cmd="${PUPPET:-puppet} config ${DEBUG+ --debug} ${VERBOSE+ --verbose} print --section ${SECTION:-user}"
-read -r -d '' cmd_awk <<_EOF
-awk -v prefix='$PREFIX' -v filter='$FILTER' -v key='$key' '
-  $awk_begin
-  \$1 ~ /Debug:/ { print; next; }
-  \$1 ~ filter {
+        ;&
+    text|yaml|json)
+        # NOTE - 'filter' and 'key' are by definition mutually exclusive!
+        read -r -d '' cmd_awk <<_EOF || true
+|awk -v prefix='$PREFIX' -v filter='$FILTER' -v key='$key' '
+    $awk_begin
+    \$1 ~ /Debug:/ { print; next; }
+    \$1 ~ filter {
         if (length(key) != 0) { \$3 = \$1; \$1 = key; }
         printf($awk_printf)
     }
 '
 _EOF
+        ;;
+    *)  >&2 echo "unsupported format ($FORMAT)"; exit 1
+
+esac
+
+# '--debug' screws up output so DO NOT use except interactive
+cmd="${PUPPET:-puppet} config ${DEBUG+ --debug} ${VERBOSE+ --verbose} print --section ${SECTION:-user}"
                 
-${DEBUG+runv} eval "$cmd $@ | $cmd_awk"
+${DEBUG+runv} eval "$cmd $@ $cmd_awk $post_format"
